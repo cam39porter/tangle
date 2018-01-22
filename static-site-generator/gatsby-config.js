fad1a6c3d8c8582548dbec978cd10da335ba6477
cam39porter <cam.b.porter@gmail.com>
2018-01-22 15:14:36 -0500

    feat(gh-pages): adding deploy functionality

--- static-site-generator/gatsby-config.js
+++ static-site-generator/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Hex Ventures',
   },
+  pathPrefix: '/reponame',
   plugins: ['gatsby-plugin-react-helmet'],
 };

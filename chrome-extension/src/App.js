e689e33d7609ca5fae27ec93d1ac5d4d320740f0
cam39porter <cam.b.porter@gmail.com>
2018-01-21 19:45:47 -0500

    feat(chrome extension): minimal chrome extension with react and tachyons

---
+++ chrome-extension/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={``}>
+        <h1 className={`f1 tc`}>Corpus</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

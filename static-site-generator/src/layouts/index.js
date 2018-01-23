6542eb94f3eda8b70e7965082b04b4debabfd6f5
cam39porter <cam.b.porter@gmail.com>
2018-01-23 13:32:15 -0500

    feat(static site): basic header and particles

--- static-site-generator/src/layouts/index.js
+++ static-site-generator/src/layouts/index.js
@@ -1,19 +1,62 @@
+/*!
+ * This contains the primary layout all pages receive.
+ */
+
 import React from 'react'
-import PropTypes from 'prop-types'
+
+import Link from 'gatsby-link'
+
+import Particles from 'react-particles-js'
+import particlesUtils from '../lib/particles-utils'
 
 import 'tachyons'
 
-const DefaultWrapper = ({ children }) => (
-  <div>
-    <header className={`f1 tc`}>Hex Ventures</header>
-    <div className={`tc`}>
+const DefaultWrapper = ({ children, data }) => (
+  <div className={`avenir pa4`}>
+
+    {/* Header */}
+    <header className={``}>
+
+      {/* Navigation */}
+      <nav className={`dt w-100 mw8 center`}>
+
+        {/* Logo */}
+        <div className={`dtc w5 v-mid pa3`}>
+          <h1 className={`f3 tc navy`}>{data.site.siteMetadata.title}</h1>
+        </div>
+
+        {/* Actions */}
+        <div className={`dtc v-mid tr pa3`}>
+          <Link className={`dim bg-navy pa3 shadow-5 f4 lightest-blue no-underline ttu`}>team</Link>
+        </div>
+      </nav>
+    </header>
+
+    {/* Children */}
+    <div className={``}>
       { children() }
     </div>
+
+    {/* Footer */}
   </div>
 )
 
-DefaultWrapper.propTypes = {
-  children: PropTypes.func,
-}
+/*!
+ * GraphQL
+ */
+
+export const query = graphql`
+  query AboutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+/*!
+ * Exports
+ */
 
 export default DefaultWrapper
--- static-site-generator/src/pages/index.js
+++ static-site-generator/src/pages/index.js
@@ -1,14 +1,21 @@
+/*!
+ * Home Page
+ */
+
 import React from 'react'
 import Link from 'gatsby-link'
+
+import Particles from 'react-particles-js'
+import particlesUtils from '../lib/particles-utils'
 
 import 'tachyons'
 
 
 const IndexPage = () => (
   <div className={``}>
-    <p className={``}>
-      This is just the beginning
-    </p>
+    <Particles 
+      params={ particlesUtils.params }
+    />
   </div>
 )
 

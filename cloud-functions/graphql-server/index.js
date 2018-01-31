ec9622a5c0bab73d96dd9de04b50128413e9c2bc
cam39porter <cam.b.porter@gmail.com>
2018-01-31 15:34:49 -0500

    fix(graphql): moving server to a directory for modularization and flexibility

--- cloud-functions/graphql-server/index.js
+++
@@ -1,9 +0,0 @@
-const graphql = require('./graphql')
-
-/**
- * GraphqQL Express function
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-exports.graphql = graphql

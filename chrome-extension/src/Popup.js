2ff75069c9c9e2165596497329e2e4200a1e4d43
Julian Griggs <julian.griggs10@gmail.com>
2018-02-04 17:47:29 -0800

    feat(chrome-ext): Add more popup ui

---
+++ chrome-extension/src/Popup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PopupListItem from './PopupListItem';
+import PopupExtras from './PopupExtras';
+class Popup extends React.Component {
+  render() {
+    return (
+      <div className={``}>
+        <h1 className={`f1 tc`}>opit</h1>
+        <div className="pa3 pa5-ns">
+          <ul className="list pl0 measure center">
+            <PopupListItem itemTitle="Bookmark Page" />
+            <PopupListItem itemTitle="View Annotations" />
+            <PopupListItem itemTitle="Etc." />
+          </ul>
+          <PopupExtras />
+        </div>
+      </div>
+    );
+  }
+}
+export default Popup;

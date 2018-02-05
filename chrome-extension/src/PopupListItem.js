23c05142740a206dffdaffa0eab9ea8e23563657
Julian Griggs <julian.griggs10@gmail.com>
2018-02-05 08:26:42 -0800

    style(chrome-ext): use template literal for classes

--- chrome-extension/src/PopupListItem.js
+++ chrome-extension/src/PopupListItem.js
@@ -3,8 +3,8 @@ import React from 'react';
 class PopupListItem extends React.Component {
   render() {
     return (
-      <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30">
-        <a href="#" className="link dim gray">
+      <li className={`lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30`}>
+        <a href="#" className={`link dim gray`}>
           {this.props.itemTitle}
         </a>
       </li>

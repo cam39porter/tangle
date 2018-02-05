e359e34b1384b06bb6236d7077c03a8259e8b6c5
Julian Griggs <julian.griggs10@gmail.com>
2018-02-04 17:47:29 -0800

    feat(chrome-ext): Add more popup ui

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-
-import 'tachyons'
-
+import React, { Component } from 'react';
+import 'tachyons';
+import './App.css';
+import Popup from './Popup';
 class App extends Component {
   render() {
     return (
-      <div className={``}>
-        <h1 className={`f1 tc`}>opit</h1>
+      <div className="App">
+        <Popup />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;

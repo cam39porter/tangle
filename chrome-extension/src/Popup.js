8fc0ee6615a079163a397ca991c4ecc0e75a55a1
Julian Griggs <julian.griggs10@gmail.com>
2018-02-05 08:26:42 -0800

    style(chrome-ext): use template literal for classes

--- chrome-extension/src/Popup.js
+++ chrome-extension/src/Popup.js
@@ -6,8 +6,8 @@ class Popup extends React.Component {
     return (
       <div className={``}>
         <h1 className={`f1 tc`}>opit</h1>
-        <div className="pa3 pa5-ns">
-          <ul className="list pl0 measure center">
+        <div className={`pa3 pa5-ns`}>
+          <ul className={`list pl0 measure center`}>
             <PopupListItem itemTitle="Bookmark Page" />
             <PopupListItem itemTitle="View Annotations" />
             <PopupListItem itemTitle="Etc." />

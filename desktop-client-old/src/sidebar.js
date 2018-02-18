241b2069db6c44512dced51094db8b64feca7220
Julian Griggs <julian.griggs10@gmail.com>
2018-02-18 14:00:34 -0800

    Remove desktop-client-old

--- desktop-client-old/src/sidebar.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Sidebar extends Component {
-  render() {
-    return <div className={`fl w-10 pa2 h-100 ba`}>Sidebar</div>;
-  }
-}
-
-export default Sidebar;

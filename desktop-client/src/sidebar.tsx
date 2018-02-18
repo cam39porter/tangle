5a2358b2ec06789ecb22e317f592b4228a7478f0
Julian Griggs <julian.griggs10@gmail.com>
2018-02-18 13:59:09 -0800

    Starter desktop-client implementation

---
+++ desktop-client/src/sidebar.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export interface Props {}
+function Sidebar(props: Props) {
+  return <div className={`fl w-10 pa2 h-100 ba`}>Sidebar</div>;
+}
+
+export default Sidebar;
---
+++ desktop-client/src/topbar.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+const logo = require("./logo.svg") as string;
+
+export interface Props {}
+function Topbar(props: Props) {
+  return (
+    <div className={`fl w-100 h3 ba`}>
+      <img src={logo} className="fl App-logo h3" alt="logo" />
+      <h1 className="fl w-80 tc">Welcome to Capture!</h1>
+    </div>
+  );
+}
+
+export default Topbar;

e4cc69efa8f1a8da142e8f0e2dcc8c9cb5ae8423
cam39porter <cam.b.porter@gmail.com>
2018-03-02 18:52:56 -0500

    feat(capture): capture hitting production server but erroring due to cors

---
+++ desktop-client/src/__generated__/types.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+//  This file was automatically generated and should not be edited.
+
+export interface CreateCaptureMutationVariables {
+  body: string,
+};
+
+export interface CreateCaptureMutation {
+  createCapture:  {
+    __typename: "Capture",
+    id: string,
+    body: string,
+  },
+};
--- desktop-client/src/index.tsx
+++ desktop-client/src/index.tsx
@@ -3,5 +3,31 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+const httpLink = createHttpLink({
+  uri: "https://opit-193719.appspot.com/graphql"
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink
+});
+
+class ApolloWrappedApp extends React.Component<object, object> {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+  }
+}
+
+ReactDOM.render(<ApolloWrappedApp />, document.getElementById(
+  "root"
+) as HTMLElement);
 registerServiceWorker();

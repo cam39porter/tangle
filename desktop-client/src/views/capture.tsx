fc57215b53f6c2785841092d4bf2947f58be2266
cam39porter <cam.b.porter@gmail.com>
2018-03-02 16:38:27 -0500

    feat(capture): basic text input with value passed to parent

--- desktop-client/src/views/capture.tsx
+++ desktop-client/src/views/capture.tsx
@@ -3,8 +3,9 @@ import * as React from "react";
 import TextInputCapture from "../components/text-input-capture";
 
 export interface Props {}
+export interface CaptureState {}
 
-class Capture extends React.Component<Props, object> {
+class Capture extends React.Component<Props, CaptureState> {
   render() {
     return (
       <div className={``}>

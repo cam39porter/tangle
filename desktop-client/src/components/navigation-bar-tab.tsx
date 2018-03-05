d643a9df674f8ac39e26fe2a9666cf00665ca12e
cam39porter <cam.b.porter@gmail.com>
2018-03-05 17:25:47 -0500

    feat(desktop client): WIP routing

--- desktop-client/src/components/navigation-bar-tab.tsx
+++ desktop-client/src/components/navigation-bar-tab.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 
+import { Link } from "react-router-dom";
+
 export interface Props {
   title: string;
 }
 
 function NavigationBarTab(props: Props) {
-  return <p className={`w-25 h1 tc dib pointer`}>{props.title}</p>;
+  return (
+    <div className={`w-25 h1 tc dib pointer`}>
+      <Link to={`/${props.title}`}>{props.title}</Link>
+    </div>
+  );
 }
 
 export default NavigationBarTab;

6a6417c66231b3be75142f41e5a63097a0cf5cd9
cam39porter <cam.b.porter@gmail.com>
2018-03-05 17:25:47 -0500

    feat(desktop client): WIP routing

--- desktop-client/src/App.tsx
+++ desktop-client/src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import "tachyons";
 
+import { Route, Switch } from "react-router-dom";
+
 import NavigationBar from "./components/navigation-bar";
 import Capture from "./views/capture";
 
@@ -9,10 +11,16 @@ class App extends React.Component<Props, object> {
   render() {
     return (
       <div className={`vh-100 w-100 avenir`}>
+        {/* Navigation Bar */}
         <div className={`clip-s`}>
           <NavigationBar />
         </div>
-        <Capture />
+
+        {/* Navigation */}
+        <Switch>
+          <Route path="/" component={Capture} />
+          <Route path="/capture" component={Capture} />
+        </Switch>
       </div>
     );
   }
--- desktop-client/src/index.tsx
+++ desktop-client/src/index.tsx
@@ -1,13 +1,18 @@
+// React
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+// Apollo
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
+
+// Router
+import { BrowserRouter as Router } from "react-router-dom";
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI
@@ -21,9 +26,11 @@ const client = new ApolloClient({
 class ApolloWrappedApp extends React.Component<object, object> {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
+      <Router>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </Router>
     );
   }
 }

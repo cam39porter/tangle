162ceaafdb1f244fac13fe330c650621a39538b0
cam39porter <cam.b.porter@gmail.com>
2018-03-05 21:53:42 -0500

    feat(desktop client): functional navigation

--- desktop-client/src/components/navigation-bar-tab.tsx
+++ desktop-client/src/components/navigation-bar-tab.tsx
@@ -1,17 +1,69 @@
 import * as React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export interface Props {
   title: string;
+  isActiveBackgroundColor: string;
+  isActiveColor: string;
 }
 
-function NavigationBarTab(props: Props) {
-  return (
-    <div className={`w-25 h1 tc dib pointer`}>
-      <Link to={`/${props.title}`}>{props.title}</Link>
-    </div>
-  );
+export interface NavigationBarTabState {
+  isActive: boolean;
+}
+
+class NavigationBarTab extends React.Component<Props, NavigationBarTabState> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isActive: false
+    };
+  }
+
+  render() {
+    return (
+      <NavLink
+        to={`/${this.props.title}`}
+        style={{
+          textDecoration: "none"
+        }}
+        isActive={(match, location) => {
+          if (match) {
+            if (!this.state.isActive) {
+              this.setState({
+                isActive: true
+              });
+            }
+            return true;
+          }
+
+          if (this.state.isActive) {
+            this.setState({
+              isActive: false
+            });
+          }
+          return false;
+        }}
+      >
+        <div
+          className={`tc pointer pa3 ttl ${
+            this.state.isActive
+              ? "bg-" + this.props.isActiveBackgroundColor
+              : "bg-light-gray"
+          }`}
+        >
+          <span
+            className={`${
+              this.state.isActive ? this.props.isActiveColor : "gray"
+            }`}
+          >
+            {this.props.title}
+          </span>
+        </div>
+      </NavLink>
+    );
+  }
 }
 
 export default NavigationBarTab;

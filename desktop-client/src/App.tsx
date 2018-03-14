984261918bbf72d5cebeaf1207b34d97f77f167a
cam39porter <cam.b.porter@gmail.com>
2018-03-14 13:23:02 -0600

    fix(desktop-client): swithcing to only keyboard shortcut instead of web app and full client (WIP)

--- desktop-client/src/App.tsx
+++ desktop-client/src/App.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import "tachyons";
 
-import { Route, Switch } from "react-router-dom";
-
-import Tangle from "./views/tangle";
 import Capture from "./views/capture";
-import Surface from "./views/surface";
-import SurfaceResults from "./views/surface-results";
-import Reflect from "./views/reflect";
 
 export interface Props {}
 
@@ -15,15 +9,7 @@ class App extends React.Component<Props, object> {
   render() {
     return (
       <div className={`vh-100 w-100 avenir`}>
-        {/* Navigation */}
-        <Switch>
-          <Route exact={true} path="/" component={Capture} />
-          <Route path="/capture" component={Capture} />
-          <Route path="/surface/:query" component={SurfaceResults} />
-          <Route path="/surface" component={Surface} />
-          <Route path="/tangle" component={Tangle} />
-          <Route path="/reflect" component={Reflect} />
-        </Switch>
+        <Capture />
       </div>
     );
   }

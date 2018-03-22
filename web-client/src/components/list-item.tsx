cfd465f4c72164eade4c733f8549903d51c9b147
cam39porter <cam.b.porter@gmail.com>
2018-03-22 17:10:47 -0400

    WIP

--- web-client/src/components/list-item.tsx
+++ web-client/src/components/list-item.tsx
@@ -9,12 +9,13 @@ export interface Props {
 class ListItem extends React.Component<Props, object> {
   render() {
     return (
-      <div className={`min-measure bg-white bb b--light-gray`}>
+      <div className={`min-measure bg-white bb b--light-gray dt`}>
         <p
-          className={`ma3 h3 measure-narrow f6 overflow-hidden overflow-scroll overflow-x-hidden`}
+          className={`dtc fl ma3 h3 measure-narrow f6 overflow-hidden overflow-scroll overflow-x-hidden`}
         >
           {this.props.body}
         </p>
+        <div className={`dtc fr ma3 h2 w2 br-100 bg-light-blue`} />
       </div>
     );
   }

854b56a10ca8ff3480811cd9c1bdf9c0dcb0f156
colemccracken <colemccracken@gmail.com>
2018-04-27 20:34:23 -0700

    handling tags

--- client-api/src/upload/services/evernote-html-parser.ts
+++ client-api/src/upload/services/evernote-html-parser.ts
@@ -5,9 +5,8 @@ import * as moment from "moment";
 
 export function parseEvernoteHtml(data): EvernoteNote {
   const $ = cheerio.load(data);
-  const tags = $("meta[name=keywords]")
-    .attr("content")
-    .split(", ");
+  const tagString = $("meta[name=keywords]").attr("content");
+  const tags = tagString ? tagString.split(", ") : [];
   const title = $("title").text();
   const created = Date.parse(
     $("meta[name=created]")

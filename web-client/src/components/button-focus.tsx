36b12642560597fa2662d677c8a855cb774375dc
cam39porter <cam.b.porter@gmail.com>
2018-05-21 15:05:58 -0400

    style(web-client): updating focus icon

--- web-client/src/components/button-focus.tsx
+++ web-client/src/components/button-focus.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 
 // Components
-import { Target } from "react-feather";
+import { Circle } from "react-feather";
 
 interface Props {
   onClick: () => void;
@@ -12,7 +12,7 @@ const ButtonFocus = (props: Props) => {
   return (
     <span className={`dt pa2 f6 tc pointer gray`} onClick={props.onClick}>
       <div className={`dtc v-mid h-100 w-100`}>
-        <Target size={16} />
+        <Circle size={16} />
       </div>
     </span>
   );

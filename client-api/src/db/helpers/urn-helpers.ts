ff5233968b57d33aa1e0a831729f3f50575ea0ca
cam39porter <cam.b.porter@gmail.com>
2018-05-23 16:53:57 -0400

    linting

--- client-api/src/db/helpers/urn-helpers.ts
+++ client-api/src/db/helpers/urn-helpers.ts
@@ -60,7 +60,7 @@ function getEntityOrTagName(urn: string): string | null {
     case "entity":
       return getName(urn);
     case "tag":
-      let name = getName(urn);
+      const name = getName(urn);
       return `#${name.slice(0, name.length - 1)}`;
     default:
       return null;

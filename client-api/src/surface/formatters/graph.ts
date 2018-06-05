ad6cabcc13b7381cb2c7658e0001e25c9601c24e
colemccracken <colemccracken@gmail.com>
2018-06-05 13:22:12 -0400

    fixing level and dup edges

--- client-api/src/surface/formatters/graph.ts
+++ client-api/src/surface/formatters/graph.ts
@@ -9,19 +9,32 @@ export function buildGraph(
   paths: Array<[Capture, Relationship, Node, Relationship, Capture]>
 ): Graph {
   const nodes = new Map<string, GraphNode>();
-  const edges = [];
+  const edges = new Map<string, Edge>();
   paths.forEach(path => {
     nodes.set(path[0].id, formatCapture(path[0], true));
     if (path[2]) {
-      nodes.set(path[2].properties["id"], formatNode(path[2]));
-      edges.push(formatEdge(path[1], path[0].id, path[2].properties["id"]));
+      if (!nodes.has(path[2].properties["id"])) {
+        nodes.set(path[2].properties["id"], formatNode(path[2]));
+      }
+      const edge = formatEdge(path[1], path[0].id, path[2].properties["id"]);
+      if (!hasEdge(edges, edge)) {
+        edges.set(formatEdgeId(edge.source, edge.destination), edge);
+      }
     }
     if (path[4]) {
-      nodes.set(path[4].id, formatCapture(path[4], false));
-      edges.push(formatEdge(path[3], path[2].properties["id"], path[4].id));
+      if (!nodes.has(path[4].id)) {
+        nodes.set(path[4].id, formatCapture(path[4], false));
+      }
+      const edge = formatEdge(path[3], path[2].properties["id"], path[4].id);
+      if (!hasEdge(edges, edge)) {
+        edges.set(formatEdgeId(edge.source, edge.destination), edge);
+      }
     }
   });
-  return new Graph(Array.from(nodes, ([, value]) => value), edges);
+  return new Graph(
+    Array.from(nodes, ([, value]) => value),
+    Array.from(edges, ([, value]) => value)
+  );
 }
 
 function formatNode(node: Node): GraphNode {
@@ -44,6 +57,16 @@ function formatCapture(capture: Capture, isRoot: boolean): GraphNode {
     capture.body,
     isRoot ? 0 : 1
   );
+}
+
+function formatEdgeId(src: string, dest: string): string {
+  return `${src};${dest}`;
+}
+
+function hasEdge(map, edge): boolean {
+  const edgeId = formatEdgeId(edge.source, edge.destination);
+  const reverseEdgeId = formatEdgeId(edge.destination, edge.source);
+  return map.has(edgeId) || map.has(reverseEdgeId);
 }
 
 function formatEdge(rel: Relationship, start: string, end: string): Edge {

f2b77579aa8a9b8227c0469d95641c5616b324c7
colemccracken <colemccracken@gmail.com>
2018-07-09 12:25:53 -0400

    rm id from appfeedback

--- client-api/src/db/services/feedback.ts
+++ client-api/src/db/services/feedback.ts
@@ -1,18 +1,11 @@
 import { executeQuery, Param } from "../db";
-import { v4 as uuidv4 } from "uuid/v4";
 import { getRequestContext } from "../../filters/request-context";
 
 export function create(body: string): Promise<boolean> {
-  const uuid = uuidv4();
   const userId = getRequestContext().user.urn;
-  const params = [
-    new Param("userId", userId.toRaw()),
-    new Param("id", uuid),
-    new Param("body", body)
-  ];
+  const params = [new Param("userId", userId.toRaw()), new Param("body", body)];
   const query = `
     CREATE (feedback:AppFeedback {
-      id: {id},
       body: {body},
       owner: {userId},
       created: TIMESTAMP()

3670bd88d30f241f8703e29a538b682e00ef6a1e
cam39porter <cam.b.porter@gmail.com>
2018-09-05 15:07:09 -0400

    adding shutdown message

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,137 +1,50 @@
-// React
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { unregister as unregisterServiceWorker } from "./registerServiceWorker";
+import { ChevronRight } from "react-feather";
 
-// Components
-import App from "./App";
-import Maintenance from "./views/maintenance";
+import "./css/custom.css";
+import "./tachyons.css";
 
-// Apollo
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { setContext } from "apollo-link-context";
-import { onError } from "apollo-link-error";
-import { RetryLink } from "apollo-link-retry";
-import { InMemoryCache, defaultDataIdFromObject } from "apollo-cache-inmemory";
-import { ApolloProvider } from "react-apollo";
-
-// Router
-import { BrowserRouter as Router } from "react-router-dom";
-
-// Config / Utils
-import { FirebaseUtils, AnalyticsUtils, ErrorsUtils } from "./utils";
-import config from "./cfg/env";
-const firebaseAuth = FirebaseUtils.firebaseAuth;
-
-// Apollo Linking
-const httpLink = createHttpLink({
-  uri: config.REACT_APP_GRAPHQL_URI,
-  useGETForQueries: true
-});
-
-const authLink = setContext((_, { headers }) => {
-  const idToken = localStorage.getItem("idToken");
-  const authorization = `Bearer ${idToken}`;
-  return {
-    headers: {
-      ...headers,
-      authorization
-    }
-  };
-});
-
-const errorLink = onError(({ networkError, graphQLErrors, operation }) => {
-  if (operation.operationName !== "reportError") {
-    if (networkError) {
-      const statusCode = networkError["statusCode"];
-      if (statusCode === 401) {
-        const user = firebaseAuth().currentUser;
-        if (user !== null) {
-          user.getIdToken(true).then(idToken => {
-            localStorage.setItem("idToken", idToken);
-          });
-        } else {
-          localStorage.removeItem("idToken");
-          firebaseAuth().signOut();
-          AnalyticsUtils.setUserId(undefined);
-        }
-      } else if (statusCode === 400) {
-        const errorMessage = `It seems that your email address is not whitelisted. Please make sure you log in with the same email you used to sign up on usetangle.com. Contact info@usetangle.com if you have any quesitons or believe this is an error.`;
-        alert(errorMessage);
-        localStorage.removeItem("idToken");
-        firebaseAuth().signOut();
-        AnalyticsUtils.setUserId(undefined);
-      }
-      ErrorsUtils.errorHandler.report(
-        networkError.message,
-        networkError.stack ? networkError.stack : {}
-      );
-    } else if (graphQLErrors) {
-      let error = graphQLErrors[0];
-      ErrorsUtils.errorHandler.report(
-        error.message,
-        error.stack ? error.stack : {}
-      );
-    }
-  }
-});
-
-const retryLink = new RetryLink({
-  delay: {
-    initial: 0,
-    max: 5000,
-    jitter: false
-  },
-  attempts: {
-    max: 2,
-    retryIf: (error, _operation) => error["statusCode"] === 401
-  }
-});
-
-const client = new ApolloClient({
-  cache: new InMemoryCache({
-    dataIdFromObject: (object: { __typename: string }) => {
-      switch (object.__typename) {
-        case "SurfaceResults":
-          return "SurfaceResults";
-        case "CaptureCollection":
-          return "CaptureCollection";
-        case "SessionItemCollection":
-          return "SessionItemCollection";
-        case "SessionCollection":
-          return "SessionCollection";
-        default:
-          return defaultDataIdFromObject(object);
-      }
-    }
-  }),
-  link: retryLink.concat(errorLink.concat(authLink.concat(httpLink)))
-});
-
-// Initialize Cloud Error Reporting
-if (process.env.REACT_APP_ENV === "production") {
-  ErrorsUtils.initializeCloudReporting(client);
-}
-
-class ApolloWrappedApp extends React.Component<object, object> {
-  render() {
-    if (process.env.REACT_APP_MAINTENANCE) {
-      return <Maintenance />;
-    }
-
-    return (
-      <Router>
-        <ApolloProvider client={client}>
-          <App />
-        </ApolloProvider>
-      </Router>
-    );
-  }
-}
-
-ReactDOM.render(<ApolloWrappedApp />, document.getElementById(
-  "root"
-) as HTMLElement);
-
-unregisterServiceWorker();
+ReactDOM.render(
+  <div className={`avenir dt vh-100 w-100 bg-near-white`}>
+    <div className={`dtc v-mid`}>
+      <div className={`center measure pa4 bg-white br4 shadow-1 tc`}>
+        <div>
+          <img
+            src="https://storage.googleapis.com/usetangle-static-assets/logo.png"
+            className={`pa2 bb bw2 b--accent`}
+            style={{
+              maxHeight: "5em"
+            }}
+          />
+          <div className={`h2`} />
+          <div className={`dt w-100 ba br4 bg-light-gray b--light-gray pa4`}>
+            <div className={`f3 measure-wide lh-copy pv4`}>
+              <div className={`f3 dark-gray fw4 pb4`}>Update</div>
+              <div className={`f4 gray fw2 tl`}>
+                After receiving feedback from our alpha users, we have decided
+                to shut down the current version Tangle. We are continuing to
+                work hard as a team, leveraging our learnings from this initial
+                test.{" "}
+                <p className={`fw4`}>
+                  Please email us if you would like your data exported as a csv
+                  or if you have any questions or comments.
+                </p>
+              </div>
+            </div>
+            <a href={`mailto:info@usetangle.com`} className={`link`}>
+              <div
+                className={`dt dim center bg-accent white bg-accent white br4 pa3 pointer`}
+              >
+                <div className={`dtc pt1`}>
+                  <span className={`f4`}>Email us</span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>,
+  document.getElementById("root") as HTMLElement
+);
